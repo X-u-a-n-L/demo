e0dafeaf46151fd0f50592e911aeafa3edc40fad
XuaN <849099134@qq.com>
2020-05-04 14:42:13 -0400

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("10");
+		System.out.println("HelloWorld");
+		System.out.println("20");
+	}
+}
